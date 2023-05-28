e3fc82cf5a09e268b9e7dc734e8639f46c0bfe8f
cristianmeelo <cristianmeelo@gmail.com>
2023-05-28 10:26:16 -0300

    :recycle: refactor: refactored styles in Team to english

--- src/components/Team/index.js
+++ src/components/Team/index.js
@@ -1,14 +1,14 @@
 import hexToRgba from "hex-to-rgba";
 import { Collaborator } from "../Colaborador";
-import "./Time.css";
+import "./index.css";
 
 export const Team = ({ name, color, collaborators, onDelete, changeColor, id, onFavorite }) => {
   return collaborators.length > 0
     ? collaborators.length > 0 && (
-        <section className="time" style={{ backgroundImage: "url(/images/fundo.png)", backgroundColor: hexToRgba(color, "0.6") }}>
+        <section className="team" style={{ backgroundImage: "url(/images/fundo.png)", backgroundColor: hexToRgba(color, "0.6") }}>
           <input value={color} type="color" className="input-color" onChange={(event) => changeColor(event.target.value, id)} />
           <h3 style={{ borderColor: color }}>{name}</h3>
-          <div className="colaboradores">
+          <div className="collaborators">
             {collaborators.map((collaborator, index) => {
               return <Collaborator onFavorite={onFavorite} backgroundColor={color} key={index} id={collaborator.id} name={collaborator.name} role={collaborator.role} image={collaborator.image} onDelete={onDelete} isFavorite={collaborator.favorite} />;
             })}
