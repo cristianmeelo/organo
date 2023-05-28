99b783eeec02498b2cbbbeb6d95ca680a9bf3531
cristianmeelo <cristianmeelo@gmail.com>
2023-05-28 14:22:24 -0300

    :bug: bugfix: solved little issue import with banner page

--- src/components/Banner/index.js
+++ src/components/Banner/index.js
@@ -3,7 +3,7 @@ import "./index.css";
 export const Banner = () => {
   return (
     <header className="banner">
-      <img src="/imagens/banner.png" alt="O banner principal da página do Organo" />
+      <img src="/images/banner.png" alt="O banner principal da página do Organo" />
     </header>
   );
 };
